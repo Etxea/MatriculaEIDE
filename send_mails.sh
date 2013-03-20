c7ad4bd5e8b384ca316b124f4df2a9e9a55db00f
Jon Latorre <moebius@etxea.net>
2013-03-20 09:39:24 +0100

    send mails

--- send_mails.sh
+++ send_mails.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-cd /var/www/virtual/matricula.eide.es/MatriculaEIDE
-/usr/bin/python manage.py send_mail
+cd /var/www/virtual/matricula.eide.es/MatriculaEIDE 
+/usr/bin/python manage.py send_mail >> ./send_mail.log 2>&1
