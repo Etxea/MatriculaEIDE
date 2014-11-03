f69e8236955015dab1f9a61512b6301c5cc3d42d
root <jonlatorremartinez@gmail.com>
2014-11-03 11:04:40 +0100

    typo

--- apps/hobetuz/models.py
+++ apps/hobetuz/models.py
@@ -147,7 +147,7 @@ class Registration(models.Model):
 		
 		message_body = html_content
 		##send_mail(subject, message_body, settings.DEFAULT_FROM_EMAIL, [self.email])
-		msg = EmailMultiAlternatives(subject, message_body, settings.DEFAULT_FROM_EMAIL, ["moebius198@gmail.com","secretaria@eide.es"])
+		msg = EmailMultiAlternatives(subject, message_body, settings.DEFAULT_FROM_EMAIL, ["moebius1984@gmail.com","secretaria@eide.es"])
 		msg.attach_alternative(html_content, "text/html")
 		##msg.content_subtype = "html"
 		msg.send()
